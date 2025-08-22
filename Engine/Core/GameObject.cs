67771ae0dc5911d9df0e34c759e0218760f35f8e
egg <egg@DESKTOP-MPJ908G>
2025-08-22 22:35:43 +0100

    adds an entity (gameobject, to be renamed later when im more awake) class, added a ; in maths.cs and made DOCUMENTATION.md for later use

---
+++ Engine/Core/GameObject.cs
@@ -0,0 +1,15 @@
+//Describes functionality for game objects in a game engine
+
+using System;
+
+namespace Engine.Core;
+
+public static class GameObject
+{
+    private static int ObjID = 0; // specific ID for each object
+    // gameobjects dont rlly need anything else rn, they just need to be identifiable
+    // also Engine.Core will be split into multiple files later for components n shit
+    // ECS engine when???
+    // maybe....
+    // okay fuck it ima go research ECS and add it, silly space game did it so i can too
+}
